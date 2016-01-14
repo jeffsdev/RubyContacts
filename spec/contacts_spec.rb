9d7b95c89d3c54544011d8638ae5c57f28989d2f
Jeff Seymour and Riley Starnes <rstarnes9@gmail.com>
2016-01-14 08:34:56 -0800

    refactor Contact class spec

--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -3,11 +3,13 @@ require('contact')
 
 describe('Contact') do
   describe("#first_name") do
-    it('returns the first name of the contact') do
+    it('returns the contact info') do
       test_contact = Contact.new({:first_name => "Bob", :last_name => "McBob", :job_title => "Salesman", :company => "Jane's Car Shack"})
       expect(test_contact.first_name()).to(eq("Bob"))
+      expect(test_contact.last_name()).to(eq("McBob"))
+      expect(test_contact.job_title()).to(eq("Salesman"))
+      expect(test_contact.company()).to(eq("Jane's Car Shack"))
     end
-
   end
 
 
