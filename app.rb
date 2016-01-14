383c32e9d2dba01aab8a1bd1e0cdcf786493cfaa
Jeff Seymour and Riley Starnes <seym1585@gmail.com>
2016-01-14 11:35:08 -0800

    start sinatra

---
+++ app.rb
@@ -0,0 +1,11 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/contact')
+require('./lib/email')
+require('./lib/mailing')
+require('./lib/phone')
+also_reload('./lib/**/*.rb')
+
+get('/') do
+  erb(:index)
+end
