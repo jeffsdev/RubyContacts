c8e5c73ffc091fc401c59f9bf04cdb4d7ca0d1d9
Jeff Seymour and Riley Starnes <rstarnes9@gmail.com>
2016-01-14 14:32:17 -0800

    update app.rb to contain form information and post to contact_list page

--- app.rb
+++ app.rb
@@ -13,3 +13,21 @@ end
 get('/contact/new') do
   erb(:contact_form)
 end
+
+get('/contact/list') do
+  @contacts = Contact.all()
+  erb(:contact_list)
+end
+
+post('/contact/list') do
+  first_name = params.fetch('first_name')
+  last_name = params.fetch('last_name')
+  job_title = params.fetch('job_title')
+  company = params.fetch('company')
+  Contact.new({:first_name => first_name,
+               :last_name => last_name,
+               :job_title => job_title,
+               :company => company}).save()
+  @contacts = Contact.all()
+  erb(:success)
+end
--- spec/contacts_integration_spec.rb
+++ spec/contacts_integration_spec.rb
@@ -10,4 +10,15 @@ describe('contact page pathways', {:type => :feature}) do
     click_link("Add Contact")
     expect(page).to have_content("Contact Form")
   end
+  it('posts name into contact_list') do
+    visit('/')
+    click_link("Add Contact")
+    fill_in('first_name', :with => 'Chad')
+    fill_in('last_name', :with => 'Brochadbro')
+    fill_in('job_title', :with => 'Broker')
+    fill_in('company', :with => 'Taco Bell')
+    click_button('Submit')
+    visit('/contact/list')
+    expect(page).to have_content("Brochadbro, Chad")
+  end
 end
